ba453359ff1ed4c64bb3cfa816e390c2f8466b49
daoducnam80 <99985087+daoducnam80@users.noreply.github.com>
2022-02-25 10:41:20 +0700

    Hello Nam

---
+++ JS/Vidu.js
@@ -0,0 +1,53 @@
+let txt = "Hello World!";
+txt = txt.
+toUpperCase()
+;
+
+let txt = "Hello World";
+txt = txt.
+replace
+("Hello", "Welcome");
+
+const fruits = ["Banana", "Orange", "Apple"];
+fruits.push("Kiwi")
+;
+
+const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
+fruits.sort()
+;
+
+const d = 
+new Date()
+;
+alert(d);
+const d = new Date();
+year = 
+d.getFullYear()
+;
+
+const d = new Date();
+month = 
+d.getMonth()
+;
+
+let r = 
+Math.random()
+;
+
+let i;
+for
+ (
+i
+ = 
+0
+; 
+i
+ < 
+10
+; 
+i++
+) {
+  console.log(i);
+}
+
+
